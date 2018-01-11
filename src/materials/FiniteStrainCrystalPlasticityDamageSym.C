007c63adf42980870007dc4018b7f802f57e6556
Nicolò Grilli <ngrilli@purdue.edu>
2018-01-11 09:04:25 -0500

    Plasticity damage with symmetric model
    Plasticity damage with symmetric model

---
+++ src/materials/FiniteStrainCrystalPlasticityDamageSym.C
@@ -0,0 +1,377 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+#include "FiniteStrainCrystalPlasticityDamageSym.h"
+#include "petscblaslapack.h"
+#include "libmesh/utility.h"
+
+template<>
+InputParameters validParams<FiniteStrainCrystalPlasticityDamageSym>()
+{
+  InputParameters params = validParams<FiniteStrainCrystalPlasticity>();
+  params.addClassDescription("Crystal Plasticity base class. Damage. Symmetric model using"
+                             "the principal values of the E (Lagrangian) strain tensor");
+  params.addRequiredCoupledVar("c","Order parameter for damage");
+  params.addParam<Real>("kdamage",1e-6,"Stiffness of damaged matrix");
+  params.addRequiredParam<Real>("n_Murnaghan", "exponent in Murnaghan EOS");
+  params.addRequiredParam<Real>("bulk_modulus_ref", "reference bulk modulus");
+  params.addRequiredParam<Real>("C0", "Von Neuman coefficient");
+  params.addRequiredParam<Real>("C1", "Landshoff coefficient");
+
+  return params;
+}
+
+FiniteStrainCrystalPlasticityDamageSym::FiniteStrainCrystalPlasticityDamageSym(const InputParameters & parameters) :
+    FiniteStrainCrystalPlasticity(parameters),
+    _c(coupledValue("c")),
+    _kdamage(getParam<Real>("kdamage")),
+    _n_Murnaghan(getParam<Real>("n_Murnaghan")),
+    _Bulk_Modulus_Ref(getParam<Real>("bulk_modulus_ref")),
+    _C0(getParam<Real>("C0")),
+    _C1(getParam<Real>("C1")),
+    _W0e(declareProperty<Real>("W0e")), // total elastic energy
+    _W0p(declareProperty<Real>("W0p")), // plastic energy (unbroken)
+    _W0p_old(getMaterialPropertyOld<Real>("W0p")), // plastic energy of previous increment (unbroken)
+    _W0p_broken(declareProperty<Real>("W0p_broken")), // plastic energy (broken)
+    _W0p_broken_old(getMaterialPropertyOld<Real>("W0p_broken")), // plastic energy of previous increment (broken)
+    _dstress_dc(declarePropertyDerivative<RankTwoTensor>(_base_name + "stress", getVar("c", 0)->name())),
+    _dW0e_dstrain(declareProperty<RankTwoTensor>("dW0e_dstrain")),
+    _dW0p_dstrain(declareProperty<RankTwoTensor>("dW0p_dstrain")),
+    _dW0p_broken_dstrain(declareProperty<RankTwoTensor>("dW0p_broken_dstrain")), // for the Jacobian calculation in the temperature kernel
+    _pk2_undamaged(declareProperty<RankTwoTensor>("pk2_undamaged")), // undamaged 2nd Piola Kirchoff Stress
+    _fe_out(declareProperty<RankTwoTensor>("fe_out")), // Elastic deformation gradient for output
+    _slip_incr_out(declareProperty<std::vector<Real>>("slip_incr_out")), // slip system strain increment for output
+    _etens(LIBMESH_DIM),
+    _epos(LIBMESH_DIM),
+    _eigval(LIBMESH_DIM)
+{
+}
+
+void
+FiniteStrainCrystalPlasticityDamageSym::preSolveStatevar()
+{
+  if (_max_substep_iter == 1)//No substepping
+  {
+    _gss_tmp = _gss_old[_qp];
+    _W0p_tmp = _W0p_old[_qp];
+    _W0p_broken_tmp = _W0p_broken_old[_qp];
+    _accslip_tmp_old = _acc_slip_old[_qp];
+  }
+  else
+  {
+    if (_first_step_iter)
+    {
+      _gss_tmp = _gss_tmp_old = _gss_old[_qp];
+      _W0p_tmp = _W0p_tmp_old = _W0p_old[_qp];
+      _W0p_broken_tmp = _W0p_broken_tmp_old = _W0p_broken_old[_qp];
+      _accslip_tmp_old = _acc_slip_old[_qp];
+    }
+    else
+    {
+      _gss_tmp = _gss_tmp_old;
+      _W0p_tmp = _W0p_tmp_old;
+      _W0p_broken_tmp = _W0p_broken_tmp_old;
+    }
+  }
+}
+
+void
+FiniteStrainCrystalPlasticityDamageSym::solveStatevar()
+{
+  Real gmax, gdiff;
+  unsigned int iterg;
+  std::vector<Real> gss_prev(_nss);
+
+  gmax = 1.1 * _gtol;
+  iterg = 0;
+
+  while (gmax > _gtol && iterg < _maxiterg) // Check for slip system resistance update tolerance
+  {
+    preSolveStress();
+    solveStress();
+    if (_err_tol)
+      return;
+
+    update_energies();
+
+    postSolveStress(); // Update _fp_old_inv = _fp_old
+
+    gss_prev = _gss_tmp;
+
+    update_slip_system_resistance(); // Update slip system resistance
+
+    gmax = 0.0;
+    for (unsigned i = 0; i < _nss; ++i)
+    {
+      gdiff = std::abs(gss_prev[i] - _gss_tmp[i]); // Calculate increment size
+
+      if (gdiff > gmax)
+        gmax = gdiff;
+    }
+    iterg++;
+  }
+
+  if (iterg == _maxiterg)
+  {
+#ifdef DEBUG
+    mooseWarning("FiniteStrainCrystalPLasticity: Hardness Integration error gmax", gmax, "\n");
+#endif
+    _err_tol = true;
+  }
+}
+
+void
+FiniteStrainCrystalPlasticityDamageSym::postSolveStatevar()
+{
+  if (_max_substep_iter == 1)//No substepping
+  {
+    _gss[_qp] = _gss_tmp;
+    _W0p[_qp] = _W0p_tmp;
+    _W0p_broken[_qp] = _W0p_broken_tmp;
+    _acc_slip[_qp] = _accslip_tmp;
+  }
+  else
+  {
+    if (_last_step_iter)
+    {
+      _gss[_qp] = _gss_tmp;
+      _W0p[_qp] = _W0p_tmp;
+      _W0p_broken[_qp] = _W0p_broken_tmp;
+      _acc_slip[_qp] = _accslip_tmp;
+    }
+    else
+    {
+      _gss_tmp_old = _gss_tmp;
+      _W0p_tmp_old = _W0p_tmp;
+      _W0p_broken_tmp_old = _W0p_broken_tmp;
+      _accslip_tmp_old = _accslip_tmp;
+    }
+  }
+}
+
+/**
+ * Old function to update slip system resistances.
+ * Kept to avoid code break at computeQpstress
+ * output slip increment
+ */
+void
+FiniteStrainCrystalPlasticityDamageSym::updateGss()
+{
+  DenseVector<Real> hb(_nss);
+  Real qab;
+
+  Real a = _hprops[4]; // Kalidindi
+
+  _slip_incr_out[_qp].resize(_nss);
+
+  _accslip_tmp = _accslip_tmp_old;
+  for (unsigned int i = 0; i < _nss; ++i)
+    _accslip_tmp += std::abs(_slip_incr(i));
+
+  for (unsigned int i = 0; i < _nss; ++i)
+    _slip_incr_out[_qp][i] = _slip_incr(i);
+
+  // Real val = std::cosh(_h0 * _accslip_tmp / (_tau_sat - _tau_init)); // Karthik
+  // val = _h0 * std::pow(1.0/val,2.0); // Kalidindi
+
+  for (unsigned int i = 0; i < _nss; ++i)
+    // hb(i)=val;
+    hb(i) = _h0 * std::pow(std::abs(1.0 - _gss_tmp[i] / _tau_sat), a) *
+            copysign(1.0, 1.0 - _gss_tmp[i] / _tau_sat);
+
+  for (unsigned int i = 0; i < _nss; ++i)
+  {
+    if (_max_substep_iter == 1) // No substepping
+      _gss_tmp[i] = _gss_old[_qp][i];
+    else
+      _gss_tmp[i] = _gss_tmp_old[i];
+
+    for (unsigned int j = 0; j < _nss; ++j)
+    {
+      unsigned int iplane, jplane;
+      iplane = i / 3;
+      jplane = j / 3;
+
+      if (iplane == jplane) // Kalidindi
+        qab = 1.0;
+      else
+        qab = _r;
+
+      _gss_tmp[i] += qab * hb(j) * std::abs(_slip_incr(j));
+      _dgss_dsliprate(i, j) = qab * hb(j) * copysign(1.0, _slip_incr(j)) * _dt;
+    }
+  }
+}
+
+// Update slip system resistance and elastic and plastic work
+void
+FiniteStrainCrystalPlasticityDamageSym::update_energies()
+{
+  RankTwoTensor cauchy_stress_undamaged, cauchy_stress, WpToTrace, WpBrokenToTrace, invFe;
+  Real detFe;
+  Real c = _c[_qp];
+
+  if (_max_substep_iter == 1) //No substepping
+  {
+    _W0p_tmp = _W0p_old[_qp];
+    _W0p_broken_tmp = _W0p_broken_old[_qp];
+  }
+  else
+  {
+    _W0p_tmp = _W0p_tmp_old;
+    _W0p_broken_tmp = _W0p_broken_tmp_old;
+  }
+
+  // Update elastic and plastic work
+  detFe = _fe.det();
+  invFe = _fe.inverse();
+
+  // _pk2[_qp] is the updated piola-kirchoff
+  cauchy_stress = _fe * _pk2[_qp] * _fe.transpose()/detFe;
+  cauchy_stress_undamaged = _fe * _pk2_undamaged[_qp] * _fe.transpose()/detFe;
+
+  WpBrokenToTrace = cauchy_stress * _fe * ( _fp_inv.inverse() - _fp_old_inv.inverse() ) * _fp_inv * invFe * detFe;
+  WpToTrace = cauchy_stress_undamaged * _fe * ( _fp_inv.inverse() - _fp_old_inv.inverse() ) * _fp_inv * invFe * detFe;
+
+  _W0p_broken_tmp += WpBrokenToTrace.trace();
+  _W0p_tmp += WpToTrace.trace();
+
+  _dW0p_broken_dstrain[_qp].zero();
+  _dW0p_dstrain[_qp].zero();
+  //_dstress_dc[_qp] = -cauchy_stress_undamaged * (2.0 * (1.0 - c));
+
+}
+
+void
+FiniteStrainCrystalPlasticityDamageSym::calcResidual( RankTwoTensor &resid )
+{
+  RankTwoTensor iden, ce, ee, ce_pk2, eqv_slip_incr, pk2_new;
+  Real trD;
+  Real c = _c[_qp];
+  Real xfac = Utility::pow<2>(1.0-c) + _kdamage;
+
+  // Isotropic elasticity is assumed
+  Real lambda = _elasticity_tensor[_qp](0, 0, 1, 1);
+  Real mu = _elasticity_tensor[_qp](0, 1, 0, 1);
+
+  iden.zero();
+  iden.addIa(1.0);
+
+  _fe = _dfgrd_tmp * _fp_prev_inv; // _fp_inv  ==> _fp_prev_inv
+  _fe_out[_qp] = _fe; // Elastic deformation gradient for output
+
+  ce = _fe.transpose() * _fe;
+  ce_pk2 = ce * _pk2_tmp;
+  ce_pk2 = ce_pk2 / _fe.det();
+
+  // Calculate Schmid tensor and resolved shear stresses
+  for (unsigned int i = 0; i < _nss; ++i)
+    _tau(i) = ce_pk2.doubleContraction(_s0[i]);
+
+  getSlipIncrements(); // Calculate dslip,dslipdtau
+
+  if (_err_tol)
+    return;
+
+  eqv_slip_incr.zero();
+  for (unsigned int i = 0; i < _nss; ++i)
+    eqv_slip_incr += _s0[i] * _slip_incr(i);
+
+  eqv_slip_incr = iden - eqv_slip_incr;
+  _fp_inv = _fp_old_inv * eqv_slip_incr;
+  _fe = _dfgrd_tmp * _fp_inv;
+
+  ce = _fe.transpose() * _fe;
+  ee = 0.5 * ( ce - iden );
+
+  ee.symmetricEigenvaluesEigenvectors(_eigval, _eigvec);
+
+  // Tensors of outerproduct of eigen vectors
+  for (unsigned int i = 0; i < LIBMESH_DIM; ++i)
+    for (unsigned int j = 0; j < LIBMESH_DIM; ++j)
+      for (unsigned int k = 0; k < LIBMESH_DIM; ++k)
+        _etens[i](j, k) = _eigvec(j, i) * _eigvec(k, i);
+
+  Real etr = 0.0;
+  for (unsigned int i = 0; i < LIBMESH_DIM; ++i)
+    etr += _eigval[i];
+
+  RankTwoTensor stress0pos;
+  for (unsigned int i = 0; i < LIBMESH_DIM; ++i)
+  {
+    stress0pos +=
+        _etens[i] * (lambda * etr + 2.0 * mu * _eigval[i]);
+  }
+
+  // Damage associated with total stress
+  pk2_new = stress0pos * xfac;
+
+  _pk2_undamaged[_qp] = stress0pos;
+
+  Real val = 0.0;
+  for (unsigned int i = 0; i < LIBMESH_DIM; ++i)
+    val += Utility::pow<2>(_eigval[i]);
+  val *= mu;
+
+  // Energy with total strain (symmetric model)
+  _W0e[_qp] = lambda * Utility::pow<2>(etr) / 2.0 + val;
+
+  // Used in PFFracBulkRate Jacobian
+  _dW0e_dstrain[_qp] = stress0pos;
+
+  // Used in StressDivergencePFFracTensors Jacobian
+  if (c < 1.0)
+    _dstress_dc[_qp] = -stress0pos * (2.0 * (1.0 - c));
+  else
+    _dstress_dc[_qp].zero();
+
+  //pk2_new.addIa(-1.0/3.0 * pk2_new.trace());
+  //pk2_new.addIa( (_Bulk_Modulus_Ref/_n_Murnaghan) * ( 1.0 - std::pow( 1.0/_fe.det() , _n_Murnaghan ) ) );
+
+  // Calculate bulk viscosity damping
+  // C0 * dot(J) / J * |dot(J) / J| + C1 * dot(J) / J
+  // C0 should be chosen of the order of rho * Le^2, rho = density, Le = element size
+  // C1 should be chosen of the order of rho * Le * cs, cs = sound speed
+  // Maheo et al. Mechanics Research Communications 38 (2011) 81 88
+  trD = ( _deformation_gradient[_qp].det() - _deformation_gradient_old[_qp].det() ) / _dt;
+  trD /= _deformation_gradient_old[_qp].det();
+
+  pk2_new.addIa( _C0 * trD * std::abs(trD) );
+  pk2_new.addIa( _C1 * trD );
+
+  resid = _pk2_tmp - pk2_new;
+}
+
+// Calculate slip increment,dslipdtau. Override to modify.
+void
+FiniteStrainCrystalPlasticityDamageSym::getSlipIncrements()
+{
+  for (unsigned int i = 0; i < _nss; ++i)
+  {
+    _slip_incr(i) = _a0(i) * std::pow(std::abs(_tau(i) / _gss_tmp[i]), 1.0 / _xm(i)) *
+                    copysign(1.0, _tau(i)) * _dt;
+    if (std::abs(_slip_incr(i)) > _slip_incr_tol * _dt)
+    {
+      _slip_incr(i) = _slip_incr_tol * _dt * copysign(1.0, _tau(i));
+      //_err_tol = true;
+#ifdef DEBUG
+      mooseWarning("Maximum allowable slip increment exceeded ", std::abs(_slip_incr(i)));
+#endif
+      return;
+    }
+  }
+
+  for (unsigned int i = 0; i < _nss; ++i)
+  {
+    _dslipdtau(i) = _a0(i) / _xm(i) *
+                    std::pow(std::abs(_tau(i) / _gss_tmp[i]), 1.0 / _xm(i) - 1.0) / _gss_tmp[i] *
+                    _dt;
+    if (std::abs(_slip_incr(i)) > _slip_incr_tol * _dt)
+    {
+      _dslipdtau(i) = 0.0;
+    }
+  }
+}
