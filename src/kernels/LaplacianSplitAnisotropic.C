787c45ecd926a31c3812bf770493304a18116240
Nicolò Grilli <ngrilli@purdue.edu>
2018-02-21 14:00:29 -0500

    beta penalty coefficient in Nguyen 2017 introduced
    beta penalty coefficient in Nguyen 2017 introduced

--- src/kernels/LaplacianSplitAnisotropic.C
+++ src/kernels/LaplacianSplitAnisotropic.C
@@ -16,11 +16,15 @@ validParams<LaplacianSplitAnisotropic>()
       "Split with a variable that holds the Laplacian of a phase field variable."
       "anisotropic Laplacian for anisotropic damage model");
   params.addRequiredCoupledVar("c", "Field variable to take the Laplacian of");
+  params.addRequiredParam<Real>("beta_penalty","penalty for damage on planes not normal to z (Nguyen, 2017)");
   return params;
 }
 
 LaplacianSplitAnisotropic::LaplacianSplitAnisotropic(const InputParameters & parameters)
-  : KernelGrad(parameters), _var_c(coupled("c")), _grad_c(coupledGradient("c"))
+  : KernelGrad(parameters),
+    _var_c(coupled("c")),
+    _grad_c(coupledGradient("c")),
+    _beta_penalty(getParam<Real>("beta_penalty"))
 {
 }
 
@@ -29,9 +33,9 @@ LaplacianSplitAnisotropic::precomputeQpResidual()
 {
   RealGradient anisotropic_grad_c;
 
-  anisotropic_grad_c(0) = _grad_c[_qp](0);
-  anisotropic_grad_c(1) = _grad_c[_qp](1);
-  anisotropic_grad_c(2) = 0.0;
+  anisotropic_grad_c(0) = (1.0 + _beta_penalty) * _grad_c[_qp](0);
+  anisotropic_grad_c(1) = (1.0 + _beta_penalty) * _grad_c[_qp](1);
+  anisotropic_grad_c(2) = _grad_c[_qp](2);
 
   return anisotropic_grad_c; // * _grad_test[_i][_qp]
 }
