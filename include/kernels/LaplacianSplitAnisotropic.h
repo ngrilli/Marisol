e0b5f168fa975cd2aebda3024ece781219e3d654
Nicolò Grilli <ngrilli@purdue.edu>
2018-02-27 14:03:37 -0500

    added normal vector to the favoured slip plane
    added normal vector to the favoured slip plane

--- include/kernels/LaplacianSplitAnisotropic.h
+++ include/kernels/LaplacianSplitAnisotropic.h
@@ -8,7 +8,7 @@
 #ifndef LAPLACIANSPLITANISOTROPIC_H
 #define LAPLACIANSPLITANISOTROPIC_H
 
-#include "KernelGrad.h"
+#include "Kernel.h"
 
 // Forward Declarations
 class LaplacianSplitAnisotropic;
@@ -20,22 +20,25 @@ InputParameters validParams<LaplacianSplitAnisotropic>();
  * Split with a variable that holds the Laplacian of the phase field.
  * anisotropic Laplacian for anisotropic damage model
  */
-class LaplacianSplitAnisotropic : public KernelGrad
+class LaplacianSplitAnisotropic : public Kernel
 {
 public:
   LaplacianSplitAnisotropic(const InputParameters & parameters);
 
 protected:
-  virtual RealGradient precomputeQpResidual();
-  virtual RealGradient precomputeQpJacobian();
-  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+  virtual Real computeQpResidual() override;
+  virtual Real computeQpJacobian() override;
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar) override;
 
 private:
   const unsigned int _var_c;
   const VariableGradient & _grad_c;
 
-  // penalty for damage on planes not normal to z (Nguyen, 2017)
+  // penalty for damage on planes not normal to the favoured cleavage plane normal (Nguyen, 2017)
   const Real _beta_penalty;
+
+  // normal to the favoured cleavage plane: M in (Nguyen, 2017)
+  const std::vector<Real> _cleavage_plane_normal;
 };
 
 #endif // LAPLACIANSPLITANISOTROPIC_H
